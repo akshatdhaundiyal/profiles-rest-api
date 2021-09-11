42f55c8aed9b383b73076071546dd2dd10187f33
Akshat_laptop <akshatdhaundiyal10@gmail.com>
2021-09-11 13:10:36 +0530

    This commit is after a long time

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+# When API calls, it uses this Response module
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ Test API View"""
+
+    def get(self, request,format=None):
+        """ Returns a list of APIView features"""
+        # To demostrate the return of some list/object in APIView
+        an_apiview = [
+        'Uses HTTPP methods as function (get,post,patch,put,delete)',
+        'Is similar to a traditional Django View',
+        'Gives you the most control over your application logic',
+        'Is mapped manually to URLs'
+        ]
+        # ^ Expecting a Response object
