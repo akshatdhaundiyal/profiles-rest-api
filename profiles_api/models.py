d3c91b43cfbcc80055ac39a14cb9636b1554df6e
Akshat_laptop <akshatdhaundiyal10@gmail.com>
2021-09-20 16:09:40 +0530

    Added POST,PUT,PATCH and DELETE

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+### Used for overriding the default permissions
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
@@ -7,17 +8,21 @@ from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profiles"""
+    """
+    Manager for user profiles
+    Usef to modify User objects and model where it is used.
+    """
 
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
-        # Normalize the input emails
+        # Normalize the input emails, capital letters to small letters
         email = self.normalize_email(email)
+        # Create new model,setting email,name
         user = self.model(email=email, name =name)
 
-        # Set hashed values for password
+        # Set hashed values for password, encrypted
         user.set_password(password)
         # Set a database, standard way of saving in django
         user.save(using=self._db)
@@ -25,8 +30,12 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self,email,name,password):
-        """ Create and save a new superuser with given details"""
+        """
+        Create and save a new superuser with given details.
+        Similar to admin user
+        """
         user = self.create_user(email,name,password)
+        # Automatically created by PermissionsMixin
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
@@ -38,7 +47,9 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """ Database model for users in the system"""
+    """
+    Database model for users in the system
+    """
     email = models.EmailField(max_length=255, unique=True)
     name  = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -62,5 +73,8 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         return self.name
 
     def __str__(self):
-        """ Return string representation of our user"""
+        """
+        Return string representation of our user.
+        Required when converting UserProfile object to string
+        """
         return self.email
